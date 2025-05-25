9c05f4b3f3bb381f09bf922f68247c82421775f5
Ghulam Mustafa <mustafaattarihafizghulam@gmail.com>
2025-05-25 22:58:58 +0500

    Weather App

---
+++ Weather App/src/App.jsx
@@ -0,0 +1,75 @@
+import styles from "./App.module.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+function App() {
+  const apiKey = `67c7a7f8dc23aea744624e4d5f7f0cf2`;
+  const [cityName, setCityName] = useState("Karachi");
+  const [apiData, setApiData] = useState(null);
+
+  useEffect(() => {
+    // getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      console.log("Fetching data...");
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
+      );
+      console.log("Data fetched successfully:", response.data);
+      setApiData(response.data);
+      console.log("city name", cityName);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(":event", event);
+    // getData()
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div>
+          <h1>Weather App !</h1>
+          <br />
+          <form action="" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Enter City "
+              onChange={(e) => {
+                setCityName(e.target.value);
+              }}
+            />
+          </form>
+          <button>Search</button>
+        </div>{" "}
+        <br />
+        <div className={styles.app}>
+          {apiData ? (
+            <div className={styles.weatherInfo}>
+              <h2>City Name : {apiData.name}</h2>
+              <span className="styles.temp">
+                <img
+                  src={`https://openweathermap.org/img/wn/${apiData.weather[0]?.icon}@2x.png`}
+                  alt="weather icon"
+                />
+                <p>Temperature: {apiData.main.temp} °C</p>
+              </span>
+              <p>Weather: {apiData.weather[0].description}</p>
+              <p>Humidity: {apiData.main.humidity}%</p>
+              <p>Wind Speed: {apiData.wind.speed} m/s</p>
+            </div>
+          ) : (
+            <p>No data available. Please enter a city name.</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
